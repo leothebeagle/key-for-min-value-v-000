16d6f4cf91d456f02f0a1c21b879ae67624fc2c4
Aziz Kosber <a.kosber@gmail.com>
2018-05-28 15:04:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -22,24 +22,4 @@ def key_for_min_value(name_hash)
 end
 end
 
-
-
-
-
-
-
-
-#ikea = {:chair => 25, :table => 85, :mattress => 450}
-#[85,450,25]
-#compare value 1 to value 2. if value 1 < value 2 then select value 1 and compare it to value 3.
-#if value 3 is less than value 1 then select that and compare it to the next value.
-
-#we could keep track of what we are comparing.
-#have a counter. so if the counter is i = 0. will keep looping while the counter is < hash.size
-
-#well come to think of it. hashes are not in order so index numbers are useless here.
-#hmm..so looks like this is what's going to happen. we will separate keys from values, and extract the values and place them in an array.
-#once that happens will then compare each member of the array and select the smallest one.
-#once the smallest one is selected, can then retrieve the hash key that corresponds to the value
-#you could potentially retireve the hash as follows. iterate once more over the hash and write a function that detects which key corresponds to that value.
-#go on little one!code your destiny!
+#this is a rather long winded approach to solving this. 
